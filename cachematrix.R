71d0fa4962e429647d58b766d050eacac0a4f5cf
Danimartfel <116942145+Danimartfel@users.noreply.github.com>
2022-10-29 10:16:06 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,43 @@
 ## Put comments here that give an overall description of what your functions do
 
 ## Write a short comment describing this function
-  #The <<- operator can be used to assign a value to an object in an 
-  #Environment that is different from the current environment.
+#The <<- operator can be used to assign a value to an object in an 
+#Environment that is different from the current environment.
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
-  set <- function(y) {
-    x <<- y
+  det <- function(z) {
+    x <<- z
     i <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set,
-       get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+  obt <- function() x
+  detinverse <- function(inverse) i <<- inverse
+  obtinverse <- function() i
+  list(det = det,
+       obt = obt,
+       detinverse = detinverse,
+       obtinverse = obtinverse)
 }
 
 
 ## Write a short comment describing this function
-    # Use "...":
-      # When it makes sense to call the function with a variable number of 
-      # arguments. 
-      # When, within your function, you call other functions, and these functions 
-      # can have a variable number of arguments, either because:
-              # the called function is generic
-              # the called function can be passed into the function as an argument
+# Use "...":
+# When it makes sense to call the function with a variable number of 
+# arguments. 
+# When, within your function, you call other functions, and these functions 
+# can have a variable number of arguments, either because:
+# the called function is generic
+# the called function can be passed into the function as an argument
 
 cacheSolve <- function(x, ...) {
-  i <- x$getinverse()
+  i <- x$obtinverse()
   if (!is.null(i)) {
     message("obtaining cached data")
     return(i)
   }
-  d <- x$get()
+  d <- x$obt()
   i <- solve(d, ...)
-  x$setinverse(i)
+  x$detinverse(i)
   i
 }
 
@@ -45,3 +45,6 @@ cacheSolve <- function(x, ...) {
 a <- matrix(c(2,3,4,5),2,2)
 A1 <- makeCacheMatrix(a)
 cacheSolve(A1)
+
+
+
